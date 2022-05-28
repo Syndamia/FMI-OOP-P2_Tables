472d7fe3f346dacb18634f2c0c95ef6ef9d2e9cc
Syndamia <kamen@syndamia.com>
2022-05-28 18:04:20 +0300

    Autocommit at 28.05.2022 @ 18:04

--- src/Models/Table.cpp
+++ src/Models/Table.cpp
@@ -1,7 +1,13 @@
 #include "Table.h"
+#include <cstring>
 
 Table::Table(unsigned rows, unsigned cols) {
 	cells = List<List<Cell*>>(rows);
 	for (unsigned i = 0; i < rows; i++)
 		cells[i] = List<Cell*>(cols);
+}
+
+void Table::putCell(unsigned row, unsigned col, const char* rawValue) {
+	unsigned intParse = atoi(rawValue);
+	unsigned doubleParse = atof(rawValue);
 }

827f0a8227b7db8ddf3f7ec0fa39bb945ea13bdb
Syndamia <kamen@syndamia.com>
2022-05-28 16:39:09 +0300

    Autocommit at 28.05.2022 @ 16:39

--- src/Models/CellFormula.cpp
+++ src/Models/CellFormula.cpp
@@ -6,16 +6,19 @@ double CellFormula::calculate(unsigned index) {
 	double lval = CellDouble(formula[index].left->getValue()).getValue();
 
 	switch(formula[index].right) {
-		plus: return lval + calculate(index++);
-		minus: return lval - calculate(index++);
-		mult: return lval * calculate(index++);
-		div: return lval / calculate(index++);
+		case plus:  return lval + calculate(index++);
+		case minus: return lval - calculate(index++);
+		case mult:  return lval * calculate(index++);
+		case div:   double rval = calculate(index++);
+			   if (rval == 0) throw std::overflow_error("Divide by zero exception");
+			   return lval / rval;
 		default: return lval;
 	}
 }
 
 String CellFormula::getValueForPrint() {
 	double result = calculate();
+
 	return "";
 }
 

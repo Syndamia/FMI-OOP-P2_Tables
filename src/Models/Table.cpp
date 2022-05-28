9e7af3dd34b747c44aeb2b29fcef7531dc83b17b
Syndamia <kamen@syndamia.com>
2022-05-28 18:24:25 +0300

    Autocommit at 28.05.2022 @ 18:24

--- src/Models/Table.cpp
+++ src/Models/Table.cpp
@@ -11,11 +11,20 @@ Table::Table(unsigned rows, unsigned cols) {
 		cells[i] = List<Cell*>(cols);
 }
 
+
+bool containsNumber(const char*& str) {
+	while (*str == ' ') str++;
+	return ((*str == '+' || *str == '-') && (*(str + 1) >= '0' && *(str + 1) <= '9')) ||
+		   (*str >= '0' && *str <= '9');
+}
+
 void Table::putCell(unsigned row, unsigned col, const char* rawValue) {
 	unsigned intParse = atoi(rawValue);
 	unsigned doubleParse = atof(rawValue);
 
 	Cell* newCell;
-	if (doubleParse - intParse > 0.00001)
+	if (doubleParse - intParse > 0.00001 && doublePrase > 0.00001)
 		newCell = new CellDouble(doubleParse);
+	else if (intParse )
+		newCell = 
 }
